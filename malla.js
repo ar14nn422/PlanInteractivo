31239b42b48d756f88127a180508c70e4ad5f701
Tu Nombre <tuemail@example.com>
2025-07-14 09:42:01 -0600

    Resto de semestres

--- malla.js
+++ malla.js
@@ -29,11 +29,35 @@ const malla = [
     
   ],
     [
-    { numero: 16, codigo: 'EIF209', nombre: 'Programación IV', creditos: 4, requisitos: ['EIF206'] },
-    { numero: 17, codigo: 'EIF211', nombre: 'Diseño e Implementación de Bases de Datos', creditos: 4,requisitos:['EIF206','EIF207'] },
-    { numero: 18, codigo: 'EIF210', nombre: 'Ingeniería en Sistemas I', creditos: 4, requisitos: ['EIF206'] },
-    { numero: 19, codigo: 'EIF212', nombre: 'Sistemas Operativos', creditos: 3, requisitos: ['EIF204','EIF205'] },
-    { numero: 20, codigo: 'EIF208', nombre: 'Comunicaciones y Redes de Computadores', creditos: 3, requisitos: ['EIF205'] },
+    { numero: 21, codigo: 'EIF209', nombre: 'Programación IV', creditos: 4, requisitos: ['EIF206'] },
+    { numero: 22, codigo: 'EIF211', nombre: 'Diseño e Implementación de Bases de Datos', creditos: 4,requisitos:['EIF206','EIF207'] },
+    { numero: 23, codigo: 'EIF210', nombre: 'Ingeniería en Sistemas I', creditos: 4, requisitos: ['EIF206'] },
+    { numero: 24, codigo: 'EIF212', nombre: 'Sistemas Operativos', creditos: 3, requisitos: ['EIF204','EIF205'] },
+    { numero: 25, codigo: 'EIF208', nombre: 'Comunicaciones y Redes de Computadores', creditos: 3, requisitos: ['EIF205'] },
+    
+  ],  
+  [
+    { numero: 26, codigo: 'EIF400', nombre: 'Paradigmas de Programación', creditos: 4, requisitos: ['EIF206'] },
+    { numero: 27, codigo: 'EIF401', nombre: 'Ingeniería en Sistemas II', creditos: 4,requisitos:['EIF210'] },
+    { numero: 28, codigo: 'EIF402', nombre: 'Administración de Bases de Datos', creditos: 4, requisitos: ['EIF212','EIF211'] },
+    { numero: 29, codigo: 'EIF407', nombre: 'Liderazgo y Organización', creditos: 3, requisitos: ['EIF404'] },
+    { numero: 30, codigo: 'EIF412', nombre: 'Investigación de Operaciones y sus Aplicaciones', creditos: 3, requisitos: ['EIF206','MAT005','MAT006'] },
+    
+  ],
+  [
+    { numero: 31, codigo: 'EIF406', nombre: 'Ingeniería en Sistemas III', creditos: 4, requisitos: ['EIF401'] },
+    { numero: 32, codigo: 'EIF411', nombre: 'Diseño y Programación de PLataformas Móviles', creditos: 4,requisitos:['EIF209'] },
+    { numero: 33, codigo: 'EIF413', nombre: 'Métodos de Investigación Científica en Informática', creditos: 3, requisitos: ['MAT006'] },
+    { numero: 34, codigo: 'EIF', nombre: 'Optativa', creditos: 3 },
+    { numero: 35, codigo: 'EIF', nombre: 'Optativa', creditos: 3},
+    
+  ],
+  [
+    { numero: 36, codigo: 'EIF408', nombre: 'Proyectos y su Aplicación en la Organización', creditos: 5, requisitos: ['EIF209','EIF401','EIF402'] },
+    { numero: 37, codigo: 'EIF409', nombre: 'Aplicaciones Informáticas Globales', creditos: 4,requisitos:['EIF209','EIF401','EIF402'] },
+    { numero: 38, codigo: 'EIF410', nombre: 'Informática y Sociedad', creditos: 2, requisitos: ['EIF401'] },
+    { numero: 39, codigo: 'EIF', nombre: 'Optativa', creditos: 3 },
+    { numero: 40, codigo: 'EIF', nombre: 'Optativa', creditos: 3},
     
   ],
 ];
